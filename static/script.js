d119aea093701f8f219aa70303d270bc2a45955f
Boffin_Omega <c.kgagangowda78@gmail.com>
2025-07-04 18:05:25 +0530

    fixed zero division error, also calc supports floating point aritmetic now

--- static/script.js
+++ static/script.js
@@ -12,29 +12,36 @@ function draw_buttons(){
             let new_key = document.createElement("div");
             new_key.className = "button";
             new_key.textContent = keypad_values[key];
+            new_key.id = keypad_values[key];
             if ("/+=*-".includes(keypad_values[key])) new_key.classList.add("o-button");
             keypad.appendChild(new_key);
         }
         
 }
 function eval(a,b,operator){
-    if(!b) return a; //when user does smt like 8+=
+    if(b===null) return a; //when user does smt like 8+=
     switch(operator){
-        case "/" : return a / b;
+        case "/" : if (b!=0) return a / b; else return "Error"
         case "+": return a + b;
         case "-": return a - b;
         case "*": return a*b;
         default : return "";
     }
 }
+function is_f(number){
+    return number.includes(".");
+}
+
 let result = null;
 let value = null;
 let operator = null;
-let op_s = false;
+let op_s = false; // stores if current operation is successful
+
 document.addEventListener("DOMContentLoaded",()=>{
     draw_buttons();
 
     let display = document.querySelector(".display");
+    let dot = document.querySelector("#\\.");
 
     document.querySelector(".clear-buttons").addEventListener("click",(e)=>{
         if (e.target.id === "ac"){
@@ -44,46 +51,56 @@ document.addEventListener("DOMContentLoaded",()=>{
         else display.textContent = display.textContent.slice(0,-1);
     })
     document.querySelector(".keypad").addEventListener("click",(e)=>{
+        if(result =="Error"){
+            display.textContent = ""; //if error occured then clear display
+            result = null;
+        } 
         //lets work with dealing with integers for now
         let key = e.target.textContent;
-        //if symbol
+        //if symbol key entered
         if("/*+-=".includes(key)){
             if(key ==="=" && !op_s){
-                //if very first operation assume valid input
-                value = parseInt(display.textContent);
+                if (is_f(display.textContent)) value = parseFloat(display.textContent);
+                else value = parseInt(display.textContent);
                 result = eval(result,value,operator);
                 display.textContent = result;
                 op_s = true;
                 value = null;
-
             }
             else{
                 if(!operator){ //first operation
                     operator = key;
-                    result = parseInt(display.textContent);
+                    if (is_f(display.textContent)) result = parseFloat(display.textContent);
+                    else result = parseInt(display.textContent);
                     display.textContent = "";
                 }
                 else{
                     if(display.textContent &&!op_s){ //deal with 8+-/ case & 8+2+-, assumes value on screen is entered value
-                        value = parseInt(display.textContent);
+                        if (is_f(display.textContent)) value = parseFloat(display.textContent);
+                        else value = parseInt(display.textContent);
                         result = eval(result,value,operator);
                         display.textContent = `${result}`;
                         op_s = true;
                         value = null;
                     }
-                    operator = key;
+                    operator = key; //to choose latest entered operator
                 }
             }
         }
-        //if numeric
+        //if numeric key was entered
         else{
-            if(op_s && value==null){
-                value = parseInt(display.textContent);
-                display.textContent = "";
-                op_s = false;
-            } 
-            display.textContent+=key;
+            if(key === "."){
+                if (!display.textContent.includes(".")) display.textContent+=key;
+            }
+            else{
+                if(op_s && value==null){
+                    if (is_f(display.textContent)) value = parseFloat(display.textContent);
+                    else value = parseInt(display.textContent);
+                    display.textContent = "";
+                    op_s = false;
+                } 
+                display.textContent+=key;
+            }
         }
-
     })
 })
