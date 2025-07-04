f2916b01858021bf5a61d15be8ef5a565d70bdd2
Boffin_Omega <c.kgagangowda78@gmail.com>
2025-07-04 15:55:21 +0530

    Successfully implemented core calculator logic

--- static/script.js
+++ static/script.js
@@ -17,13 +17,73 @@ function draw_buttons(){
         }
         
 }
+function eval(a,b,operator){
+    if(!b) return a; //when user does smt like 8+=
+    switch(operator){
+        case "/" : return a / b;
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a*b;
+        default : return "";
+    }
+}
+let result = null;
+let value = null;
+let operator = null;
+let op_s = false;
 document.addEventListener("DOMContentLoaded",()=>{
     draw_buttons();
 
     let display = document.querySelector(".display");
+
     document.querySelector(".clear-buttons").addEventListener("click",(e)=>{
-        if (e.target.id === "ac") display.textContent = "";
+        if (e.target.id === "ac"){
+            display.textContent = "";
+            result = value = operator= null;
+        }
         else display.textContent = display.textContent.slice(0,-1);
     })
+    document.querySelector(".keypad").addEventListener("click",(e)=>{
+        //lets work with dealing with integers for now
+        let key = e.target.textContent;
+        //if symbol
+        if("/*+-=".includes(key)){
+            if(key ==="=" && !op_s){
+                //if very first operation assume valid input
+                value = parseInt(display.textContent);
+                result = eval(result,value,operator);
+                display.textContent = result;
+                op_s = true;
+                value = null;
 
+            }
+            else{
+                if(!operator){ //first operation
+                    operator = key;
+                    result = parseInt(display.textContent);
+                    display.textContent = "";
+                }
+                else{
+                    if(display.textContent &&!op_s){ //deal with 8+-/ case & 8+2+-, assumes value on screen is entered value
+                        value = parseInt(display.textContent);
+                        result = eval(result,value,operator);
+                        display.textContent = `${result}`;
+                        op_s = true;
+                        value = null;
+                    }
+                    operator = key;
+                }
+            }
+        }
+        //if numeric
+        else{
+            if(op_s && value==null){
+                value = parseInt(display.textContent);
+                display.textContent = "";
+                op_s = false;
+            } 
+            display.textContent+=key;
+        }
+
+    })
 })
